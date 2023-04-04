eb815b674f4231ce7a4c09b96601d30f8c3d2bb9
siandrev <andreev.sergey.i@yandex.ru>
2023-04-04 19:21:56 +0400

    fix: build fixes

--- src/entities/balance/model/interfaces/index.ts
+++ src/entities/balance/model/interfaces/index.ts
@@ -1,4 +1,1 @@
-export type { BillingHistory } from './billing-history';
-export type { Payment } from './payment';
 export type { Refill } from './refill';
-export type { IPaymentDescription } from './i-payment-description';
