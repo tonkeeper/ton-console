7b37e41c1203421dcf1247d0a7008bf92e0a3c70
Alexander Mescheryakov <alexander.m@tonkeeper.com>
2023-04-26 14:25:35 +0400

    Revert "fix: refills disabled"
    Issues are resolved. Restoring payments

--- src/entities/balance/ui/RefillModal.tsx
+++ src/entities/balance/ui/RefillModal.tsx
@@ -10,28 +10,30 @@ import {
     ModalOverlay,
     Text
 } from '@chakra-ui/react';
-import { H4 } from 'src/shared';
+import { CopyPad, createTransferLink, H4, Pad } from 'src/shared';
+import { QRCodeSVG } from 'qrcode.react';
+import { balanceStore } from '../model';
 import { observer } from 'mobx-react-lite';
 
 const RefillModal: FunctionComponent<{
     isOpen: boolean;
     onClose: () => void;
 }> = props => {
-    //  const depositAddress = balanceStore.depositAddress$.value;
+    const depositAddress = balanceStore.depositAddress$.value;
     return (
         <Modal scrollBehavior="inside" size="md" {...props}>
             <ModalOverlay />
             <ModalContent>
                 <ModalHeader>
                     <H4 mb="1">Balance Refill</H4>
-                    {/* <Text textStyle="body2" color="text.secondary">
+                    <Text textStyle="body2" color="text.secondary">
                         Scan QR code for refill or send Toncoin by address bellow. Send only Toncoin
                         on this address. Sending other coins may result in permanent loss.
-                    </Text>*/}
+                    </Text>
                 </ModalHeader>
                 <ModalCloseButton />
                 <ModalBody pt="0" pb="4">
-                    {/*{depositAddress && (
+                    {depositAddress && (
                         <>
                             <Pad mb="4" display="flex" alignItems="center" justifyContent="center">
                                 <QRCodeSVG
@@ -42,8 +44,7 @@ const RefillModal: FunctionComponent<{
                             </Pad>
                             <CopyPad text={depositAddress} />
                         </>
-                    )}*/}
-                    <Text>Payments are currently not available. Please try again later.</Text>
+                    )}
                 </ModalBody>
 
                 <ModalFooter gap="3" pt="0">
