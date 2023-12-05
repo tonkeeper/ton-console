9bb305a06545794d1fdc5a1deee003d0ba039be9
siandreev <andreev.sergey.i@yandex.ru>
2023-12-05 12:17:00 +0300

    fix(TC-505): [TON Analytics - History] Сломалась кнопка New Request

--- src/pages/analytics/history/index.tsx
+++ src/pages/analytics/history/index.tsx
@@ -4,17 +4,20 @@ import { H4, Overlay } from 'src/shared';
 import { AnalyticsHistoryTable, analyticsHistoryTableStore } from 'src/features';
 import { Link } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
+import { projectsStore } from 'src/entities';
 
 const HistoryPage: FunctionComponent<ComponentProps<typeof Box>> = () => {
     useEffect(() => {
         analyticsHistoryTableStore.loadFirstPage();
     }, []);
 
+    const queryAllowed = projectsStore.selectedProject?.capabilities.stats.query;
+
     return (
         <Overlay display="flex" flexDirection="column">
             <Flex align="flex-start" justify="space-between" mb="5">
                 <H4>History</H4>
-                <Button as={Link} to="../query" variant="secondary">
+                <Button as={Link} to={queryAllowed ? '../query' : '../graph'} variant="secondary">
                     New Request
                 </Button>
             </Flex>
