a4d5a9052c7c900adb2c669b43c0872742624d4f
siandreev <andreev.sergey.i@yandex.ru>
2024-01-12 18:03:52 +0100

    fix: invoices table heading updated

--- src/features/invoices/ui/table/RefreshInvoicesTableButton.tsx
+++ src/features/invoices/ui/table/RefreshInvoicesTableButton.tsx
@@ -1,23 +1,19 @@
 import { ComponentProps, FunctionComponent } from 'react';
 import { Button } from '@chakra-ui/react';
 import { observer } from 'mobx-react-lite';
-import { RefreshIcon16 } from 'src/shared';
+import { IconButton, RefreshIcon16 } from 'src/shared';
 import { invoicesTableStore } from 'src/features';
 
 const RefreshInvoicesTableButton: FunctionComponent<ComponentProps<typeof Button>> = props => {
     return (
-        <Button
-            textStyle="body2"
-            color="text.secondary"
+        <IconButton
             onClick={() =>
                 invoicesTableStore.loadFirstPageWithNewParams({ cancelPreviousCall: true })
             }
-            variant="flat"
+            icon={<RefreshIcon16 />}
+            aria-label="refresh table"
             {...props}
-        >
-            <RefreshIcon16 mr="2" />
-            Update
-        </Button>
+        />
     );
 };
 
