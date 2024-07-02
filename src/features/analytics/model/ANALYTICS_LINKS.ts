4c8b720c9464885f5c74a52626f4495ba797a867
Moiseev Ilya <ilya@mois.pro>
2024-07-02 15:28:30 +0400

    feat: update analytics links to docs

--- src/features/analytics/model/ANALYTICS_LINKS.ts
+++ src/features/analytics/model/ANALYTICS_LINKS.ts
@@ -1,7 +1,10 @@
 export const ANALYTICS_LINKS = {
     INTRO: 'https://docs.tonconsole.com/analytics',
     GRAPH: {
-        HOW_IT_WORKS: 'https://docs.tonconsole.com/analytics/graph'
+        HOW_IT_WORKS: 'https://docs.tonconsole.com/analytics#building-graph'
+    },
+    QUERY: {
+        INTRO: 'https://docs.tonconsole.com/analytics#query'
     },
     JOIN_CONTACT: 'https://t.me/subden'
 };
