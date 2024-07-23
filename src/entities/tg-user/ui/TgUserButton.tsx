9bb70b89f141825de22acda3d5477a8fe66c8d05
Moiseev Ilya <ilya@mois.pro>
2024-07-23 13:40:22 +0400

    feat: add userId to TgUserButton component

--- src/entities/tg-user/ui/TgUserButton.tsx
+++ src/entities/tg-user/ui/TgUserButton.tsx
@@ -1,7 +1,8 @@
-import { FunctionComponent } from 'react';
+import { FC } from 'react';
 import { observer } from 'mobx-react-lite';
-import { tGUserStore } from 'src/entities';
+import { TgUser, tGUserStore } from 'src/entities';
 import {
+    Box,
     Button,
     HStack,
     Image,
@@ -9,52 +10,59 @@ import {
     MenuItem,
     MenuList,
     Text,
-    useBreakpointValue
+    Tooltip,
+    useBreakpointValue,
+    useClipboard
 } from '@chakra-ui/react';
-import { ArrowIcon, DisconnectIcon, MenuButtonDefault, TgIcon } from 'src/shared';
+import { ArrowIcon, DisconnectIcon, MenuButtonDefault, Span, TgIcon } from 'src/shared';
 
-export const TgUserButton: FunctionComponent = observer(() => {
+const ExistUserMenu: FC<{ user: TgUser }> = observer(({ user }) => {
+    const { hasCopied, onCopy } = useClipboard(user.id.toString());
+
+    return (
+        <Menu placement="bottom">
+            <MenuButtonDefault w="200px" rightIcon={<ArrowIcon />}>
+                <HStack spacing="2">
+                    {user.imageUrl && <Image w="7" h="7" borderRadius="100" src={user.imageUrl} />}
+                    <Text textStyle="label2" noOfLines={1}>
+                        {user.firstName} {user.lastName}
+                    </Text>
+                </HStack>
+            </MenuButtonDefault>
+            <MenuList w="216px">
+                <Box fontSize={14} paddingX={3}>
+                    My user ID:{' '}
+                    <Tooltip closeOnClick={false} label={hasCopied ? 'Copied!' : 'Copy'}>
+                        <Span fontFamily={'mono'} onClick={onCopy} _hover={{ cursor: 'pointer' }}>
+                            {user.id}
+                        </Span>
+                    </Tooltip>
+                </Box>
+                <MenuItem onClick={() => tGUserStore.logout()}>
+                    <DisconnectIcon mr="2" />
+                    <Text textStyle="label2">Disconnect</Text>
+                </MenuItem>
+            </MenuList>
+        </Menu>
+    );
+});
+
+export const TgUserButton: FC = observer(() => {
     const buttonText = useBreakpointValue({
         base: 'Connect',
         md: 'Connect via Telegram'
     });
 
-    if (tGUserStore.user$.value) {
-        return (
-            <Menu placement="bottom">
-                <MenuButtonDefault w="200px" rightIcon={<ArrowIcon />}>
-                    <HStack spacing="2">
-                        {tGUserStore.user$.value.imageUrl && (
-                            <Image
-                                w="7"
-                                h="7"
-                                borderRadius="100"
-                                src={tGUserStore.user$.value.imageUrl}
-                            />
-                        )}
-                        <Text textStyle="label2" noOfLines={1}>
-                            {tGUserStore.user$.value.firstName} {tGUserStore.user$.value.lastName}
-                        </Text>
-                    </HStack>
-                </MenuButtonDefault>
-                <MenuList w="216px">
-                    <MenuItem onClick={() => tGUserStore.logout()}>
-                        <DisconnectIcon mr="2" />
-                        <Text textStyle="label2">Disconnect</Text>
-                    </MenuItem>
-                </MenuList>
-            </Menu>
-        );
-    } else {
-        return (
-            <Button
-                isLoading={tGUserStore.user$.isLoading}
-                leftIcon={<TgIcon />}
-                onClick={() => tGUserStore.login()}
-                variant="secondary"
-            >
-                {buttonText}
-            </Button>
-        );
-    }
+    return tGUserStore.user$.value ? (
+        <ExistUserMenu user={tGUserStore.user$.value} />
+    ) : (
+        <Button
+            isLoading={tGUserStore.user$.isLoading}
+            leftIcon={<TgIcon />}
+            onClick={() => tGUserStore.login()}
+            variant="secondary"
+        >
+            {buttonText}
+        </Button>
+    );
 });
