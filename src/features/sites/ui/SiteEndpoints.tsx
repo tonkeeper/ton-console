40095485716058806d7886ffabda30ebaebd6b41
Moiseev Ilya <ilya@mois.pro>
2024-07-25 19:39:44 +0400

    feat: add form for change site endpoint

--- src/features/sites/ui/SiteEndpoints.tsx
+++ src/features/sites/ui/SiteEndpoints.tsx
@@ -1,15 +1,74 @@
-import { Box, Text } from '@chakra-ui/react';
+import {
+    Box,
+    Button,
+    ButtonGroup,
+    FormControl,
+    FormErrorMessage,
+    Input,
+    chakra
+} from '@chakra-ui/react';
 import { observer } from 'mobx-react-lite';
-import { FC } from 'react';
+import { FC, useCallback } from 'react';
 import { H4 } from 'src/shared';
+import { Site, sitesStore } from '../model/sites.store';
+import { useForm } from 'react-hook-form';
 
-const SiteEndpoints: FC = () => {
+interface SiteEndpointFormValues {
+    endpoint: string;
+}
+
+const SiteEndpoints: FC<{ site: Site; onClose: () => void }> = ({ site, onClose }) => {
+    const formId = 'site-endpoints-form';
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<SiteEndpointFormValues>();
+
+    const submitHandler = useCallback(
+        async (form: SiteEndpointFormValues) => {
+            try {
+                await sitesStore.updateEndpoints(site.id, [form.endpoint]).then(() => onClose());
+            } catch (e) {
+                console.error(e);
+            }
+        },
+        [site]
+    );
+
     return (
         <Box paddingY={5}>
             <H4>Endpoint</H4>
-            <Text color="text.secondary" fontSize={14}>
-                TODO
-            </Text>
+            <chakra.form
+                id={formId}
+                maxW="480px"
+                paddingY={3}
+                onSubmit={handleSubmit(submitHandler)}
+                noValidate
+            >
+                <FormControl isInvalid={!!errors.endpoint} isRequired>
+                    <Input
+                        autoComplete="off"
+                        autoFocus
+                        defaultValue={site.endpoints[0]}
+                        id="endpoint"
+                        placeholder="http://15.29.87.01:433 or https://example.com"
+                        {...register('endpoint', {
+                            required: 'This is required'
+                        })}
+                    />
+
+                    <FormErrorMessage pos="static">{errors.endpoint?.message}</FormErrorMessage>
+                </FormControl>
+            </chakra.form>
+            <ButtonGroup mt={3} spacing={3}>
+                <Button form={formId} type="submit">
+                    Save
+                </Button>
+                <Button onClick={onClose} variant="secondary">
+                    Cancel
+                </Button>
+            </ButtonGroup>
         </Box>
     );
 };
