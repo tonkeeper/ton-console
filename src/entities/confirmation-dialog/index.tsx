06cc6ba5caf00fc637a2f06e32edd1f5f29251e4
Moiseev Ilya <ilya@mois.pro>
2024-08-27 11:42:27 +0400

    chore: updated design ConfirmationDialog

--- src/entities/confirmation-dialog/index.tsx
+++ src/entities/confirmation-dialog/index.tsx
@@ -1,53 +1,92 @@
 import {
-    AlertDialog,
-    AlertDialogBody,
-    AlertDialogContent,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogOverlay,
-    Button
+    Button,
+    Input,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    Text
 } from '@chakra-ui/react';
-import { FC, useRef } from 'react';
+import { FC, ReactNode, useEffect, useState } from 'react';
 
 export const ConfirmationDialog: FC<{
     isOpen: boolean;
     onClose: () => void;
     onConfirm: () => void;
-    title: string;
-    description?: string;
-    cancelText?: string;
-    confirmText?: string;
+    title: ((confirmValue: string) => string) | string;
+    confirmValue?: string;
+    description?: ((confirmValue: string) => ReactNode) | ReactNode;
+    cancelButtonText?: string;
+    confirmButtonText?: string;
+    isLoading?: boolean;
 }> = ({
     isOpen,
     onClose,
     onConfirm,
     title,
     description,
-    cancelText = 'Cancel',
-    confirmText = 'Confirm'
+    confirmValue,
+    cancelButtonText = 'Cancel',
+    confirmButtonText = 'Confirm',
+    isLoading = false
 }) => {
-    const cancelRef = useRef(null);
+    const [inputValue, setInputValue] = useState('');
+
+    useEffect(() => {
+        if (!isOpen) {
+            setInputValue('');
+        }
+    }, [isOpen]);
+
+    const titleString = typeof title === 'function' ? confirmValue && title(confirmValue) : title;
+    const descriptionString =
+        typeof description === 'function' ? confirmValue && description(confirmValue) : description;
 
     return (
-        <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
-            <AlertDialogOverlay>
-                <AlertDialogContent>
-                    <AlertDialogHeader fontSize="lg" fontWeight="bold">
-                        {title}
-                    </AlertDialogHeader>
+        <Modal isOpen={isOpen} onClose={onClose} scrollBehavior="inside" size="md">
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader textAlign="center">{titleString}</ModalHeader>
+                <ModalCloseButton />
+                {descriptionString && (
+                    <ModalBody pb="0">
+                        <Text
+                            textStyle="text.body2"
+                            mb="6"
+                            color="text.secondary"
+                            textAlign="center"
+                        >
+                            {descriptionString}
+                        </Text>
+                        {confirmValue && (
+                            <Input
+                                onChange={e => setInputValue(e.target.value)}
+                                placeholder="Name"
+                                value={inputValue}
+                            />
+                        )}
+                    </ModalBody>
+                )}
 
-                    {description && <AlertDialogBody paddingY={0}>{description}</AlertDialogBody>}
-
-                    <AlertDialogFooter>
-                        <Button ref={cancelRef} onClick={onClose} variant="outline">
-                            {cancelText}
-                        </Button>
-                        <Button ml={3} onClick={onConfirm}>
-                            {confirmText}
-                        </Button>
-                    </AlertDialogFooter>
-                </AlertDialogContent>
-            </AlertDialogOverlay>
-        </AlertDialog>
+                <ModalFooter gap="3">
+                    <Button flex={1} onClick={onClose} variant="secondary">
+                        {cancelButtonText}
+                    </Button>
+                    <Button
+                        flex={1}
+                        isDisabled={!!confirmValue && inputValue !== confirmValue}
+                        isLoading={isLoading}
+                        onClick={onConfirm}
+                        type="submit"
+                        variant="primary"
+                    >
+                        {confirmButtonText}
+                    </Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
     );
 };
