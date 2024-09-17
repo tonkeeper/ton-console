df30b62989b8f07dcf6064a5c21aa9eff6cce25e
Moiseev Ilya <ilya@mois.pro>
2024-09-17 22:38:20 +0400

    feat: add search input to Jetton Minter page

--- src/pages/jetton/minter/index.tsx
+++ src/pages/jetton/minter/index.tsx
@@ -3,19 +3,15 @@ import { TonConnectButton } from '@tonconnect/ui-react';
 import { observer } from 'mobx-react-lite';
 import { FC } from 'react';
 import { ButtonLink, H4, Overlay } from 'src/shared';
+import { SearchInput } from './ui/SearchInput';
 
 const MinterPage: FC<BoxProps> = () => {
     return (
         <Overlay display="flex" flexDirection="column">
             <Flex align="flex-start" gap={4} mb="5">
                 <H4>Jetton Minter</H4>
-                <ButtonLink
-                    ml="auto"
-                    href="/jetton/minter/new"
-                    // size="md"
-                >
-                    New Jetton
-                </ButtonLink>
+                <SearchInput />
+                <ButtonLink href="/jetton/minter/new">New Jetton</ButtonLink>
                 <TonConnectButton />
             </Flex>
         </Overlay>
---
+++ src/pages/jetton/minter/ui/SearchInput.tsx
@@ -0,0 +1,75 @@
+import {
+    Button,
+    Input,
+    InputGroup,
+    InputLeftElement,
+    InputRightElement,
+    useToast
+} from '@chakra-ui/react';
+import { Address } from '@ton/core';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { CloseIcon24, IconButton, SearchIcon24 } from 'src/shared';
+
+export const SearchInput = () => {
+    const [isSearchFocused, setIsSearchFocused] = useState(false);
+    const [searchValue, setSearchValue] = useState('');
+    const navigate = useNavigate();
+    const toast = useToast();
+
+    const handleSearch = () => {
+        try {
+            const address = Address.parse(searchValue.trim());
+
+            navigate(`/jetton/minter/view?address=${address.toString()}`);
+        } catch (error) {
+            toast({
+                title: 'Invalid address',
+                status: 'error',
+                duration: 5000,
+                position: 'bottom-left',
+                isClosable: true
+            });
+        }
+    };
+
+    return (
+        <InputGroup
+            w={isSearchFocused ? '533px' : '320px'}
+            ml="auto"
+            transition="width 0.2s"
+            size="md"
+        >
+            <InputLeftElement pointerEvents="none">
+                <SearchIcon24 color="text.secondary" />
+            </InputLeftElement>
+            <Input
+                pr="100px"
+                onBlur={() => setTimeout(() => setIsSearchFocused(false), 100)}
+                onChange={e => setSearchValue(e.target.value)}
+                onFocus={() => setIsSearchFocused(true)}
+                placeholder="Search..."
+                value={searchValue}
+            />
+            <InputRightElement gap={1} display="flex" w="100px" ml="auto">
+                {searchValue && (
+                    <IconButton
+                        aria-label="clear search"
+                        icon={<CloseIcon24 />}
+                        onClick={() => setSearchValue('')}
+                    />
+                )}
+                {searchValue && (
+                    <Button
+                        h="1.75rem"
+                        colorScheme="constant.white"
+                        onClick={handleSearch}
+                        size="sm"
+                    >
+                        Find
+                    </Button>
+                )}
+            </InputRightElement>
+        </InputGroup>
+    );
+};
