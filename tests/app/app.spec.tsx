c85dc0ed3b6a99b5c4fd0703da1a7b28381b6cbf
Moiseev Ilya <ilya@mois.pro>
2024-09-19 12:09:43 +0400

    Small fix base test

--- tests/app/app.spec.tsx
+++ tests/app/app.spec.tsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import { render } from '@testing-library/react';
 
-import App from 'src/app';
+import App from '../../src/app';
 
 describe('App', () => {
     it('Should be rendered', () => {
