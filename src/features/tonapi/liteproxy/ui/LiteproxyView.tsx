9bec302d7e71cbd4dce2af9560f909914e86975e
Moiseev Ilya <ilya@mois.pro>
2024-11-06 10:30:08 +0400

    chore: change layout

--- src/features/tonapi/liteproxy/ui/LiteproxyView.tsx
+++ src/features/tonapi/liteproxy/ui/LiteproxyView.tsx
@@ -1,32 +1,30 @@
-import { TableContainer, Text, Flex } from '@chakra-ui/react';
-import { ComponentProps, FC } from 'react';
+import { Text, Flex } from '@chakra-ui/react';
+import { FC } from 'react';
 import { CopyPad } from 'src/shared';
 import { liteproxysStore } from '../model';
 import { observer } from 'mobx-react-lite';
 
-const LiteproxyView: FC<ComponentProps<typeof TableContainer>> = () => {
+const LiteproxyView: FC = () => {
     const liteproxyList = liteproxysStore.liteproxyList$.value;
 
     return (
         <>
-            <Text textStyle="text.label1" mb={2} fontSize={18} fontWeight={600}>
-                Servers
-            </Text>
-            {liteproxyList.length > 0 &&
-                liteproxyList.map((liteproxy, index) => (
-                    <Flex key={liteproxy.server} direction="column" gap={1} mb={4}>
-                        <Flex align="baseline" gap={2}>
-                            <Text textStyle="text.label2">
-                                {index === 0 ? 'Primary' : 'Backup'}:
-                            </Text>
-                            <CopyPad text={liteproxy.server} variant="flat" width="190px" />
-                        </Flex>
-                        <Flex align="baseline" gap={2}>
-                            <Text textStyle="text.label2">Public key:</Text>
-                            <CopyPad text={liteproxy.public_key} variant="flat" width="400px" />
-                        </Flex>
+            {liteproxyList.map((liteproxy, index) => (
+                <Flex key={liteproxy.server} direction="column" gap={1} mb={4}>
+                    <Text textStyle="text.label1" fontWeight={600}>
+                        {index === 0 ? 'Primary' : 'Backup'}
+                    </Text>
+
+                    <Flex align="baseline" gap={2}>
+                        <Text textStyle="text.label2">Server:</Text>
+                        <CopyPad text={liteproxy.server} variant="flat" width="190px" />
                     </Flex>
-                ))}
+                    <Flex align="baseline" gap={2}>
+                        <Text textStyle="text.label2">Public key:</Text>
+                        <CopyPad text={liteproxy.public_key} variant="flat" width="400px" />
+                    </Flex>
+                </Flex>
+            ))}
         </>
     );
 };
