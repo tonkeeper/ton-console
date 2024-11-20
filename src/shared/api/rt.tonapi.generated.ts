81bdb6e64bd70a29ddcd5ef7d9e86ab3d5b5aaa0
Moiseev Ilya <ilya@mois.pro>
2024-11-20 19:32:37 +0400

    feat: added network management for webhooks

--- src/shared/api/rt.tonapi.generated.ts
+++ src/shared/api/rt.tonapi.generated.ts
@@ -14,12 +14,35 @@ export interface RTError {
     error: string;
 }
 
+export interface RTUsageStats {
+    /** @format date-time */
+    period_started_at: string;
+    /** @format date-time */
+    period_ended_at?: string;
+    /** @format int64 */
+    account_subscriptions: number;
+    /** @format int64 */
+    webhooks_delivered: number;
+    /** @format int64 */
+    webhooks_failed: number;
+}
+
 export interface RTWebhookList {
     webhooks: {
         /** @format int64 */
         id: number;
         endpoint: string;
+        token: string;
         subscribed_accounts: number;
+        subscribed_msg_opcodes: number;
+        subscribed_to_mempool: boolean;
+        subscribed_to_new_contracts: boolean;
+        status: RTWebhookListStatusEnum;
+        /** @format date-time */
+        status_updated_at: string;
+        /** @format date-time */
+        last_online_at: string;
+        status_failed_attempts: number;
     }[];
 }
 
@@ -35,6 +58,17 @@ export interface RTWebhookAccountTxSubscriptions {
         /** @format int64 */
         failed_attempts?: number;
     }[];
+}
+
+export interface RTWebhookMsgOpcodeSubscriptions {
+    subscriptions: {
+        opcode: string;
+    }[];
+}
+
+export enum RTWebhookListStatusEnum {
+    RTOnline = 'online',
+    RTOffline = 'offline'
 }
 
 import type {
@@ -213,6 +247,7 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
                 /** @example "NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODQ3..." */
                 token?: string;
                 project_id: string;
+                network?: 'mainnet' | 'testnet'
             },
             params: RequestParams = {}
         ) =>
@@ -235,6 +270,7 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
                 /** @example "NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODQ3..." */
                 token?: string;
                 project_id: string;
+                network?: 'mainnet' | 'testnet'
             },
             data: {
                 endpoint: string;
@@ -245,6 +281,8 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
                 {
                     /** @format int64 */
                     webhook_id: number;
+                    /** secret token which will be used by TONAPI to authenticate itself when sending notifications to the webhook endpoint */
+                    token: string;
                 },
                 RTError
             >({
@@ -268,12 +306,66 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
                 /** @example "NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODQ3..." */
                 token?: string;
                 project_id: string;
+                network?: 'mainnet' | 'testnet'
             },
             params: RequestParams = {}
         ) =>
             this.request<any, RTError>({
                 path: `/webhooks/${webhookId}`,
                 method: 'DELETE',
+                query: query,
+                format: 'json',
+                ...params
+            }),
+
+        /**
+         * @description generate a new secret token which will be used by TONAPI to authenticate itself when sending notifications to the webhook endpoint. The old token will be invalidated.
+         *
+         * @name WebhookGenerateNewToken
+         * @request POST:/webhooks/{webhook_id}/generate-new-token
+         */
+        webhookGenerateNewToken: (
+            webhookId: number,
+            query: {
+                /** @example "NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODQ3..." */
+                token?: string;
+                project_id: string;
+                network?: 'mainnet' | 'testnet'
+            },
+            params: RequestParams = {}
+        ) =>
+            this.request<
+                {
+                    token: string;
+                },
+                RTError
+            >({
+                path: `/webhooks/${webhookId}/generate-new-token`,
+                method: 'POST',
+                query: query,
+                format: 'json',
+                ...params
+            }),
+
+        /**
+         * @description Let TONAPI know that webhook is ready to receive notifications after it has been unavailable
+         *
+         * @name WebhookBackOnline
+         * @request POST:/webhooks/{webhook_id}/back-online
+         */
+        webhookBackOnline: (
+            webhookId: number,
+            query: {
+                /** @example "NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODQ3..." */
+                token?: string;
+                project_id: string;
+                network?: 'mainnet' | 'testnet'
+            },
+            params: RequestParams = {}
+        ) =>
+            this.request<any, RTError>({
+                path: `/webhooks/${webhookId}/back-online`,
+                method: 'POST',
                 query: query,
                 format: 'json',
                 ...params
@@ -291,6 +383,7 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
                 /** @example "NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODQ3..." */
                 token?: string;
                 project_id: string;
+                network?: 'mainnet' | 'testnet'
             },
             data: {
                 accounts: {
@@ -320,6 +413,7 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
                 /** @example "NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODQ3..." */
                 token?: string;
                 project_id: string;
+                network?: 'mainnet' | 'testnet'
             },
             data: {
                 accounts: string[];
@@ -347,6 +441,7 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
                 /** @example "NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODQ3..." */
                 token?: string;
                 project_id: string;
+                network?: 'mainnet' | 'testnet'
                 /** @default 0 */
                 offset?: number;
                 /**
@@ -359,6 +454,207 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
         ) =>
             this.request<RTWebhookAccountTxSubscriptions, RTError>({
                 path: `/webhooks/${webhookId}/account-tx/subscriptions`,
+                method: 'GET',
+                query: query,
+                format: 'json',
+                ...params
+            }),
+
+        /**
+         * No description
+         *
+         * @name WebhookNewContractsSubscriptions
+         * @request GET:/webhooks/{webhook_id}/msg-opcode/subscriptions
+         */
+        webhookNewContractsSubscriptions: (
+            webhookId: number,
+            query: {
+                /** @example "NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODQ3..." */
+                token?: string;
+                project_id: string;
+                network?: 'mainnet' | 'testnet'
+                /** @default 0 */
+                offset?: number;
+                /**
+                 * @max 1000
+                 * @default 1000
+                 */
+                limit?: number;
+            },
+            params: RequestParams = {}
+        ) =>
+            this.request<RTWebhookMsgOpcodeSubscriptions, RTError>({
+                path: `/webhooks/${webhookId}/msg-opcode/subscriptions`,
+                method: 'GET',
+                query: query,
+                format: 'json',
+                ...params
+            }),
+
+        /**
+         * @description receive a notification when a new contract is deployed to the blockchain
+         *
+         * @name WebhookNewContractSubscribe
+         * @request POST:/webhooks/{webhook_id}/subscribe-new-contracts
+         */
+        webhookNewContractSubscribe: (
+            webhookId: number,
+            query: {
+                /** @example "NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODQ3..." */
+                token?: string;
+                project_id: string;
+                network?: 'mainnet' | 'testnet'
+            },
+            params: RequestParams = {}
+        ) =>
+            this.request<any, RTError>({
+                path: `/webhooks/${webhookId}/subscribe-new-contracts`,
+                method: 'POST',
+                query: query,
+                format: 'json',
+                ...params
+            }),
+
+        /**
+         * @description receive a notification when a new contract is deployed to the blockchain
+         *
+         * @name WebhookNewContractUnsubscribe
+         * @request POST:/webhooks/{webhook_id}/unsubscribe-new-contracts
+         */
+        webhookNewContractUnsubscribe: (
+            webhookId: number,
+            query: {
+                /** @example "NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODQ3..." */
+                token?: string;
+                project_id: string;
+                network?: 'mainnet' | 'testnet'
+            },
+            params: RequestParams = {}
+        ) =>
+            this.request<any, RTError>({
+                path: `/webhooks/${webhookId}/unsubscribe-new-contracts`,
+                method: 'POST',
+                query: query,
+                format: 'json',
+                ...params
+            }),
+
+        /**
+         * @description receive a notification when there is a new message with a given opcode
+         *
+         * @name WebhookMsgOpcodeSubscribe
+         * @request POST:/webhooks/{webhook_id}/subscribe-msg-opcode/{opcode}
+         */
+        webhookMsgOpcodeSubscribe: (
+            webhookId: number,
+            opcode: string,
+            query: {
+                /** @example "NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODQ3..." */
+                token?: string;
+                project_id: string;
+                network?: 'mainnet' | 'testnet'
+            },
+            params: RequestParams = {}
+        ) =>
+            this.request<any, RTError>({
+                path: `/webhooks/${webhookId}/subscribe-msg-opcode/${opcode}`,
+                method: 'POST',
+                query: query,
+                format: 'json',
+                ...params
+            }),
+
+        /**
+         * @description stop receiving a notification about a new message with a given opcode
+         *
+         * @name WebhookMsgOpcodeUnsubscribe
+         * @request POST:/webhooks/{webhook_id}/unsubscribe-msg-opcode/{opcode}
+         */
+        webhookMsgOpcodeUnsubscribe: (
+            webhookId: number,
+            opcode: string,
+            query: {
+                /** @example "NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODQ3..." */
+                token?: string;
+                project_id: string;
+                network?: 'mainnet' | 'testnet'
+            },
+            params: RequestParams = {}
+        ) =>
+            this.request<any, RTError>({
+                path: `/webhooks/${webhookId}/unsubscribe-msg-opcode/${opcode}`,
+                method: 'POST',
+                query: query,
+                format: 'json',
+                ...params
+            }),
+
+        /**
+         * @description subscribe to notifications for mempool messages
+         *
+         * @name WebhookMempoolSubscribe
+         * @request POST:/webhooks/{webhook_id}/mempool/subscribe
+         */
+        webhookMempoolSubscribe: (
+            webhookId: number,
+            query: {
+                /** @example "NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODQ3..." */
+                token?: string;
+                project_id: string;
+                network?: 'mainnet' | 'testnet'
+            },
+            params: RequestParams = {}
+        ) =>
+            this.request<any, RTError>({
+                path: `/webhooks/${webhookId}/mempool/subscribe`,
+                method: 'POST',
+                query: query,
+                format: 'json',
+                ...params
+            }),
+
+        /**
+         * No description
+         *
+         * @name WebhookMempoolUnsubscribe
+         * @request POST:/webhooks/{webhook_id}/mempool/unsubscribe
+         */
+        webhookMempoolUnsubscribe: (
+            webhookId: number,
+            query: {
+                /** @example "NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODQ3..." */
+                token?: string;
+                project_id: string;
+                network?: 'mainnet' | 'testnet'
+            },
+            params: RequestParams = {}
+        ) =>
+            this.request<any, RTError>({
+                path: `/webhooks/${webhookId}/mempool/unsubscribe`,
+                method: 'POST',
+                query: query,
+                format: 'json',
+                ...params
+            })
+    };
+    usageStats = {
+        /**
+         * No description
+         *
+         * @name GetUsageStats
+         * @request GET:/usage-stats
+         */
+        getUsageStats: (
+            query: {
+                /** @example "NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODQ3..." */
+                token?: string;
+                project_id: string;
+                network?: 'mainnet' | 'testnet'
+            },
+            params: RequestParams = {}
+        ) =>
+            this.request<RTUsageStats, RTError>({
+                path: `/usage-stats`,
                 method: 'GET',
                 query: query,
                 format: 'json',
