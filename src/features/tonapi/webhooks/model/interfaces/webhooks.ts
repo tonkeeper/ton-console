439b2eeed7cf8b2528567d330d007581143b1f39
Moiseev Ilya <ilya@mois.pro>
2024-11-21 14:29:53 +0400

    feat: add status & functionality for back to online for webhook

--- src/features/tonapi/webhooks/model/interfaces/webhooks.ts
+++ src/features/tonapi/webhooks/model/interfaces/webhooks.ts
@@ -1,8 +1,11 @@
+import { RTWebhookListStatusEnum } from 'src/shared/api/streaming-api';
+
 export interface Webhook {
     id: number;
     endpoint: string;
     subscribed_accounts: number;
     token: string;
+    status: RTWebhookListStatusEnum;
 }
 
 export type WebhooksStatType = 'delivered' | 'failed';
