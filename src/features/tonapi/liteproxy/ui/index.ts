427b2fb4c325ca72cd1709474b39b2340c912469
Moiseev Ilya <ilya@mois.pro>
2025-10-22 03:31:25 +0400

    feat: add Liteproxy statistics modal and integrate with LiteproxyView

--- src/features/tonapi/liteproxy/ui/index.ts
+++ src/features/tonapi/liteproxy/ui/index.ts
@@ -1,2 +1,3 @@
 export { default as CreateLiteproxyModal } from './CreateLiteproxyModal';
 export { default as LiteproxysTable } from './LiteproxyView';
+export { default as LiteproxyView } from './LiteproxyView';
