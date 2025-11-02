c9317a40592c56dfb3ff15eac0946d0f40c68fe2
Moiseev Ilya <ilya@mois.pro>
2025-11-02 15:35:01 +0400

    refactor: enhance payment link creation in RefillModalContent component
    - Updated the `createTransferLink` function to require specific options, improving type safety and clarity.
    - Simplified the payment link generation logic in `RefillModalContent` by consolidating conditions and ensuring all necessary parameters are included.
    - Improved the toast notification formatting for better readability of transaction details.
    
    These changes streamline the payment process and enhance the user experience in the RefillModal.

--- src/shared/lib/blockchain/transfer.ts
+++ src/shared/lib/blockchain/transfer.ts
@@ -1,44 +1,17 @@
-import BigNumber from 'bignumber.js';
-import { TokenCurrencyAmount } from 'src/shared';
-
 export function createTransferLink(
     address: string,
-    options?: {
-        amount?: string | number | BigNumber | TokenCurrencyAmount;
-        text?: string;
-        exp?: Date | number | string;
-        jetton?: string;
+    options: {
+        amount: string;
+        text: string;
+        jetton: string;
     }
 ): string {
     const baseUrl = 'https://app.tonkeeper.com/transfer';
     const link = new URL(address ? `${baseUrl}/${address}` : baseUrl);
 
-    if (options?.jetton) {
-        link.searchParams.append('jetton', options.jetton);
-    }
-
-    if (options?.amount) {
-        let value: string;
-        if (typeof options.amount === 'object' && 'amount' in options.amount) {
-            value = options.amount.weiAmount.toFixed(0);
-        } else {
-            value = new BigNumber(options.amount).toFixed(0);
-        }
-        link.searchParams.append('amount', value);
-    }
-
-    if (options?.text) {
-        link.searchParams.append('text', encodeURIComponent(options.text));
-    }
-
-    if (options?.exp) {
-        link.searchParams.append(
-            'exp',
-            options.exp instanceof Date
-                ? Math.floor(options.exp.getTime() / 1000).toString()
-                : options.exp.toString()
-        );
-    }
+    link.searchParams.append('jetton', options.jetton);
+    link.searchParams.append('amount', options.amount);
+    link.searchParams.append('text', options.text);
 
     return link.toString();
 }
