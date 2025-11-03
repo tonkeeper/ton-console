310707c1662b1429c01b71e3349d2a273a978dbf
Moiseev Ilya <ilya@mois.pro>
2025-11-03 23:03:50 +0400

    refactor: remove unused AppMessagesPayment and AppMessagesStats interfaces
    - Deleted the `AppMessagesPayment` and `AppMessagesStats` interfaces as they are no longer needed in the application.
    - Updated the index file to reflect these removals, streamlining the codebase and improving maintainability.
    
    These changes help reduce clutter in the code and ensure that only relevant interfaces are retained.

--- src/features/app-messages/model/interfaces/index.ts
+++ src/features/app-messages/model/interfaces/index.ts
@@ -1,3 +1,1 @@
 export type { AppMessagesPackage } from './app-messages-package';
-export type { AppMessagesPayment } from './app-messages-payment';
-export type { AppMessagesStats } from './app-messages-stats';
