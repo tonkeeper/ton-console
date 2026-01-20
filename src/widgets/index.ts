7fcdcf41a15d1408a9bc7b5f194d4d43192fab6d
Moiseev Ilya <ilya@mois.pro>
2026-01-20 14:04:04 +0400

    chore: remove unused files and dead code

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,5 +1,4 @@
 export { Header } from './header';
 export { Footer } from './footer';
 export { default as Aside } from './aside';
-export { CreateSubscriptionsPlan } from './create-subscriptions-plan';
 export { MobileNavDrawer } from './mobile-nav-drawer';
