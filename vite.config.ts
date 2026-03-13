0ee0c6a3db868f99fe88fb3eeeca8314bc9166ae
max <qwertynestor40@gmail.com>
2026-03-13 18:50:34 +0300

    wip: update dependency, remove sudo password

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 /// <reference types="vite/client" />
 
 import { defineConfig, loadEnv } from 'vite';
-import mkcert from 'vite-plugin-mkcert';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { createHtmlPlugin } from 'vite-plugin-html';
@@ -20,7 +19,6 @@ export default ({ mode }) => {
         plugins: [
             tsconfigPaths(),
             react(),
-            mkcert(),
             createHtmlPlugin({
                 entry: '/src/main.tsx',
                 inject: {
@@ -53,7 +51,7 @@ export default ({ mode }) => {
         ],
         server: {
             host: '127.0.0.1', // Need fix for bot validate URL
-            port: 5201,
+            port: 3000,
             watch: {
                 usePolling: true
             },
